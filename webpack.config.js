6fbca94d535fe9ad069128a80c3be7c0bf27c7b9
w87498112 <87498112@qq.com>
2017-01-23 20:53:23 +0800

    dddddddddd

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var webpack=require('webpak'),path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './app/main.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  }
+};
